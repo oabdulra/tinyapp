fd110465abf94195134e2798111afaaf09bc2775
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-05-01 17:35:58 -0400

    made sure the routing worked

--- express_server.js
+++ express_server.js
@@ -52,6 +52,10 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
